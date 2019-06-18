985a86c88b0230f4cb7b31b92a66ed415db60d5e
venomyea <venomyea@hotmail.com>
2019-06-18 20:50:34 +0800

    first commit

---
+++ src/main/java/idv/yeh/demo/Demo.java
@@ -0,0 +1,12 @@
+package idv.yeh.demo;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		System.out.println("這是git測試");
+		System.out.println("這是git測試");
+		System.out.println("這是git測試");
+		System.out.println("這是git測試");
+	}
+
+}
