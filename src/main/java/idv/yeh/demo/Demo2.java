6c8e34f81fa85e083e998ff01126bf1181fb969e
venomyea <venomyea@hotmail.com>
2019-06-18 22:24:33 +0800

    add demi2

---
+++ src/main/java/idv/yeh/demo/Demo2.java
@@ -0,0 +1,23 @@
+package idv.yeh.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo2 {
+	public static void main(String[] args) {
+		String a = "a";
+		String b = "b";
+		String c = "c";
+		String d = "d";
+		String e = "e";
+
+		List<String> list = new ArrayList<String>();
+		list.add(a);
+		list.add(b);
+		list.add(c);
+		list.add(d);
+		list.add(e);
+
+		System.out.println(list);
+	}
+}
