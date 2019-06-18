3a7f80621d8d4d727929734109192c0c55cc0620
GIACHIYEH <38279589+GIACHIYEH@users.noreply.github.com>
2019-06-18 21:06:31 +0800

    新增merge文字

--- src/main/java/idv/yeh/demo/Demo.java
+++ src/main/java/idv/yeh/demo/Demo.java
@@ -3,7 +3,7 @@ package idv.yeh.demo;
 public class Demo {
 	
 	public static void main(String[] args) {
-		System.out.println("這是git測試");
+		System.out.println("這是git測試 merge測試");
 		
 		for (int i = 0; i < 100; i++) {
 			System.out.println(i+2);
